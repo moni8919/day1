3d533cda1dc5abf4f3ceb10d5bd37fbec20c7c67
moni8919 <chinthaparthimounika050@gmail.com>
2024-12-04 14:39:17 +0530

    Add files via upload

---
+++ big1.java
@@ -0,0 +1,15 @@
+public class big1 {
+    public static void main(String[]args){
+        int a=20;
+        int b=100;
+        if(a>b)
+        {
+            System.out.println("a is greater");
+        }
+        else
+        {
+            System.out.println("b is greater");
+
+        }
+    }
+}
