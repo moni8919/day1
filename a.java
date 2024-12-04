0a195839d9ef0cb88a84101880127d5a74fe18f5
moni8919 <chinthaparthimounika050@gmail.com>
2024-12-04 14:28:32 +0530

    Add files via upload

---
+++ a.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class a{
+    public static void main(String[]args){
+        int a=12;
+        int b=56;
+        int c=90;
+        if(a>b&a>c)
+        {
+            System.out.println(a);
+        }else if(b>a&b>c)
+        {
+            System.out.println(b);
+        }
+        {if(c>a&c>b)
+            {
+            System.out.println(c);
+            }
+    }
+        }
+    }
