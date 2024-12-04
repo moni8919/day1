04506f6bdd171cfe29522f898cfb90d5dd856fef
moni8919 <chinthaparthimounika050@gmail.com>
2024-12-04 15:03:58 +0530

    Add files via upload

---
+++ multiple.java
@@ -0,0 +1,12 @@
+public class multiple {
+    public static void main(String[]args){
+        int a=16;
+        if((a&3)==0){
+            System.out.println("multiple of 4 ");
+        }
+        else{
+            System.out.println(" not multiple of 4 ");
+        }
+    }
+    
+}
