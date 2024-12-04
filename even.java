b1a77ae1f33aef56ea7a313d9b4b3d1036a4c0f6
moni8919 <chinthaparthimounika050@gmail.com>
2024-12-04 16:22:01 +0530

    Add files via upload

---
+++ even.java
@@ -0,0 +1,11 @@
+public class even {
+    public static void main(String[] args) {
+        int a=4;
+        if((a&1)==0){
+            System.out.println("even");
+        }else{
+            System.out.println("odd");
+        }
+    }
+    
+}
